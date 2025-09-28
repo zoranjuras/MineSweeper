5a3a506de89f9513b99359037424fecbeb6abf27
zoranjuras <cmundir@gmail.com>
2025-09-28 20:51:46 +0200

    Create main window with text panel and label; add board panel with grid. Add private MineTile class and populate grid.

---
+++ src/main/java/Minesweeper.java
@@ -0,0 +1,65 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Minesweeper {
+
+    private class MineTile extends JButton {
+        int row;
+        int col;
+
+        public MineTile (int row, int col) {
+            this.row = row;
+            this.col = col;
+        }
+    }
+
+    int tileSize = 70;
+    int numRows = 8;
+    int numCols = numRows;
+    int boardWidth = numCols * tileSize;
+    int boardHeight = numRows * tileSize;
+
+    JFrame frame = new JFrame("Minesweeper");
+    JLabel textLabel = new JLabel();
+    JPanel textPanel = new JPanel();
+    JPanel boardPanel = new JPanel();
+
+    MineTile[][] board = new MineTile[numRows][numCols];
+
+
+    public Minesweeper() {
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+
+        textLabel.setFont(new Font("Arial", Font.BOLD, 25));
+        textLabel.setHorizontalAlignment(JLabel.CENTER);
+        textLabel.setText("Minesweeper");
+        textLabel.setOpaque(true);
+
+        textPanel.setLayout(new BorderLayout());
+        textPanel.add(textLabel);
+        frame.add(textPanel, BorderLayout.NORTH);
+
+        boardPanel.setLayout(new GridLayout(numRows, numCols));
+//        boardPanel.setBackground(Color.GREEN);
+        frame.add(boardPanel);
+
+        for (int r = 0; r < numRows; r++) {
+            for (int c = 0; c < numCols; c++) {
+                MineTile tile = new MineTile(r, c);
+                board[r][c] = tile;
+
+                tile.setFocusable(false);
+                tile.setMargin(new Insets(0, 0, 0, 0));
+                tile.setFont(new Font("Arial Unicode MS", Font.PLAIN, 45));
+                tile.setText("1");
+                boardPanel.add(tile);
+            }
+        }
+
+    }
+}
